ff6b5c293df0de2fd68683e57c3a082b63e2637e
unknown <vitkot@gmail.com>
2019-10-02 03:00:47 +0300

    laba1_done

---
+++ src/main/java/laba1/App.java
@@ -0,0 +1,33 @@
+package laba1;
+
+import laba1.controller.Controller;
+import laba1.model.PassCoachService;
+import laba1.model.TrainService;
+import laba1.view.View;
+
+import java.io.InputStream;
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args )
+    {
+        View view = new View();
+        Controller controller = new Controller(view, System.in);
+        controller.processUserInput();
+
+        /*PassCoachService passCoachService = PassCoachService.getInstance();
+        TrainService trainService = TrainService.getInstance(passCoachService);
+        passCoachService.fillDefaultListOfCoaches();
+        trainService.fillDefaultTrain();
+
+        System.out.println(passCoachService.getAllCoaches());
+        System.out.println(trainService.getTrains());*/
+
+
+    }
+}
